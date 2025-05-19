eb2b1027943788e35d0b3a374ba395ec97f0df21
Steven Bossio Leal <bossiolealbl@gmail.com>
2025-05-18 19:37:18 -0500

    Adds message upload functionality
    Enables message creation with file uploads.
    
    Implements file handling in the message controller using Multer.
    Updates dependencies for file uploading and exception handling.
    Registers a global exception filter for RPC calls.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
+import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(
+    new RpcCustomExceptionFilter()
+  )
   await app.listen(envs.port);
 }
 bootstrap();
--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,16 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { MESSAGE_SERVICE } from 'src/config';
 import { catchError } from 'rxjs';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('message')
 export class MessageController {
   constructor(@Inject(MESSAGE_SERVICE) private readonly messageService: ClientProxy) { }
 
   @Post()
-  create(@Body() createMessageDto: CreateMessageDto) {
-    return this.messageService.send('createMessage', createMessageDto)
+  @UseInterceptors(FilesInterceptor('files', 10))
+  create(@UploadedFiles() files: Express.Multer.File[], @Body() createMessageDto: CreateMessageDto) {
+    // if(files){
+    //   const metaFiles = files.map(file => (
+    //      {
+    //       filename: file.filename,
+    //       mimetype: file.mimetype,
+    //       size: file.size,
+
+    //     }
+    //   ))
+    // }
+    return this.messageService.send('createMessage', {
+      createMessageDto,
+      files: files || []
+    })
       .pipe(
         catchError(error => { throw new RpcException(error) }
         )
